8c3a7061a432c4a24566f5c6856256883780e834
DwightWells <sw4915284@gmail.com>
2025-11-03 21:51:28 +0800

    home page scaffold

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import { ScoreNFTDemo } from "@/components/ScoreNFTDemo";
+
+export default function Home() {
+  return (
+    <main className="">
+      <div className="flex flex-col gap-8 items-center sm:items-start w-full px-3 md:px-0">
+        <ScoreNFTDemo />
+      </div>
+    </main>
+  );
+}
+
